cbef1257e20408b895319ff50ab51829247b4adb
unknown <38155769+timtam8520@users.noreply.github.com>
2018-06-11 13:43:22 +1000

    feat: productionising app, using minification, cache busting & separate js and css files

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,19 @@
 export default function getBaseUrl() {
-	const inDevelopment = window.location.hostname === 'localhost';
-	return inDevelopment ? 'http://localhost:3001/' : '/';
+	return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+function getQueryStringParameterByName(name, url) {
+	if (!url) {
+		url = window.location.href;
+	}
+	name = name.replace(/[\[\]]/g, '\\$&');
+	const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+	const results = regex.exec(url);
+	if (!results) {
+		return null;
+	} else if (!results[2]) {
+		return '';
+	} else {
+		return decodeURIComponent(results[2].replace('/\+/g', " "));
+	}
 }
---
+++ src/vendor.js
@@ -0,0 +1,9 @@
+/**
+ * This file contains references to the vendor libraries
+ * we're using in this project. This is used by webpack
+ * in the production build only.
+ */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
